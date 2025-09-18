ad906723b51d48d8f800ec3b1103f987eb9d20ce
Gabriels <gabriels@arqs.co>
2025-09-18 12:32:56 -0500

    Se añade isRunning

--- src/main/java/com/dashboard_gk/dashboard_gk/controller/UserController.java
+++ src/main/java/com/dashboard_gk/dashboard_gk/controller/UserController.java
@@ -10,10 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/dashboard-gk-backend/user-controller")
@@ -64,6 +61,7 @@ public class UserController {
         }
     }
 
+    @GetMapping("/is-running")
     public String isRunning() {
         return "Hola Mundo!!";
     }
